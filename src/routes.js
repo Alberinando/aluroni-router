284eeba45f468104cc46a97d75428b69e81ee285
Alberinando <80666910+Alberinando@users.noreply.github.com>
2023-05-30 10:34:53 -0300

    Criando o NotFound

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
+import Footer from 'components/Footer';
 import PaginaPadrao from 'components/PaginaPadrao';
 import Menu from 'components/menu';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
+import NotFound from 'pages/NotFound';
 import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -14,10 +16,12 @@ export default function AppRouter() {
           <Route path='/' element={<PaginaPadrao />}>
             <Route index element={<Inicio/>} />
             <Route path='cardapio' element={<Cardapio />} />
-            <Route path='sobre' element={ <Sobre/> } />
+            <Route path='sobre' element={<Sobre />} />
           </Route>
+          <Route path='*' element={ <NotFound/> } />
         </Routes>
       </Router>
+      <Footer />
     </main>
   );
 }
